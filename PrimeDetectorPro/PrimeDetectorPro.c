e6cec9c6be102ebe008a01b9e64db2232c106e37
Hredleaf <redleaf_net@outlook.com>
2026-06-22 18:39:12 +0800

    Updated a prime factorization function.

--- PrimeDetectorPro/PrimeDetectorPro.c
+++ PrimeDetectorPro/PrimeDetectorPro.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <intrin.h>
 #include <Windows.h>
+#include <conio.h>
 #include <stdio.h>
 
 __forceinline uint64_t __stdcall MultiplyModulo(const uint64_t LeftInput, const uint64_t RightInput, const uint64_t Modulo)
@@ -191,6 +192,157 @@ __forceinline bool __stdcall IsPrime(uint64_t Number)
     }
 }
 
+typedef struct BasePrimeFactors
+{
+    uint64_t Primality[16];
+    uint64_t PrimalityExponent[16];
+    uint64_t PrimalityCount;
+} PrimeFactors;
+
+__forceinline PrimeFactors __stdcall PrimeFactorize(uint64_t Number)
+{
+    uint64_t PrimeArray[64];
+    uint64_t PrimeArrayNumber = 0;
+
+    if (~Number & 1)
+    {
+        *PrimeArray = 2;
+
+        while (~Number & 1)
+        {
+            PrimeArray[PrimeArrayNumber] = 2;
+            ++PrimeArrayNumber;
+
+            Number >>= 1;
+        }
+    }
+
+    if (IsPrime(Number))
+    {
+        PrimeFactors Result;
+
+        Result.PrimalityCount = 2;
+        *Result.Primality = 2;
+        *Result.PrimalityExponent = PrimeArrayNumber;
+        Result.Primality[1] = Number;
+        Result.PrimalityExponent[1] = 1;
+
+        return Result;
+    }
+
+    if (Number == 1)
+    {
+        PrimeFactors Result;
+
+        Result.PrimalityCount = 1;
+        *Result.Primality = 2;
+        *Result.PrimalityExponent = PrimeArrayNumber;
+
+        return Result;
+    }
+
+    uint64_t TemporaryFactors[16];
+    *TemporaryFactors = Number;
+    uint64_t TemporaryFactorsNumber = 1;
+    uint64_t CurrentDecomposeNumber;
+
+    while (TemporaryFactorsNumber)
+    {
+        --TemporaryFactorsNumber;
+        CurrentDecomposeNumber = TemporaryFactors[TemporaryFactorsNumber];
+
+        for (uint64_t C = 1; C < CurrentDecomposeNumber; ++C)
+        {
+            uint64_t X = 2;
+            uint64_t Y = 2;
+            uint64_t CurrentGCD = 1;
+
+            while (CurrentGCD == 1)
+            {
+                X = MultiplyModulo(X, X, CurrentDecomposeNumber) + C;
+                if (X >= CurrentDecomposeNumber) X -= CurrentDecomposeNumber;
+
+                Y = MultiplyModulo(Y, Y, CurrentDecomposeNumber) + C;
+                if (Y >= CurrentDecomposeNumber) Y -= CurrentDecomposeNumber;
+                Y = MultiplyModulo(Y, Y, CurrentDecomposeNumber) + C;
+                if (Y >= CurrentDecomposeNumber) Y -= CurrentDecomposeNumber;
+
+                CurrentGCD = GCD((X > Y) ? (X - Y) : (Y - X), CurrentDecomposeNumber);
+            }
+
+            if (CurrentGCD != CurrentDecomposeNumber)
+            {
+                if (IsPrime(CurrentGCD))
+                {
+                    PrimeArray[PrimeArrayNumber] = CurrentGCD;
+                    ++PrimeArrayNumber;
+                }
+                else
+                {
+                    TemporaryFactors[TemporaryFactorsNumber] = CurrentGCD;
+                    ++TemporaryFactorsNumber;
+                }
+                uint64_t OtherFactor = CurrentDecomposeNumber / CurrentGCD;
+                if (IsPrime(OtherFactor))
+                {
+                    PrimeArray[PrimeArrayNumber] = OtherFactor;
+                    ++PrimeArrayNumber;
+                }
+                else
+                {
+                    TemporaryFactors[TemporaryFactorsNumber] = OtherFactor;
+                    ++TemporaryFactorsNumber;
+                }
+
+                break;
+            }
+        }
+    }
+
+    {
+        PrimeFactors Result;
+        Result.PrimalityCount = 0;
+
+        uint64_t ProcessedMark = 0xFFFFFFFFFFFFFFFFULL;
+
+        while (1)
+        {
+            uint64_t Minimum = ProcessedMark;
+            bool Found = false;
+
+            for (uint64_t K = 0; K < PrimeArrayNumber; ++K)
+            {
+                if (PrimeArray[K] != ProcessedMark && PrimeArray[K] < Minimum)
+                {
+                    Minimum = PrimeArray[K];
+                    Found = true;
+                }
+            }
+
+            if (!Found)
+            {
+                break;
+            }
+
+            uint64_t Count = 0;
+            for (uint64_t K = 0; K < PrimeArrayNumber; ++K)
+            {
+                if (PrimeArray[K] == Minimum)
+                {
+                    ++Count;
+                    PrimeArray[K] = ProcessedMark;
+                }
+            }
+
+            Result.Primality[Result.PrimalityCount] = Minimum;
+            Result.PrimalityExponent[Result.PrimalityCount] = Count;
+            ++Result.PrimalityCount;
+        }
+
+        return Result;
+    }
+}
+
 int32_t main()
 {
     uint64_t Number;
@@ -202,21 +354,28 @@ int32_t main()
     QueryPerformanceFrequency(&CounterFrequency);
     QueryPerformanceCounter(&StartCount);
 
-    bool IsNumberPrime = IsPrime(Number);
+    PrimeFactors PrimalitiesArray = PrimeFactorize(Number);
 
     QueryPerformanceCounter(&EndCount);
-    DurationNanoseconds = (EndCount.QuadPart - StartCount.QuadPart) * 1000000 / CounterFrequency.QuadPart;
+    DurationNanoseconds = (EndCount.QuadPart - StartCount.QuadPart) * 1000000.0 / CounterFrequency.QuadPart;
 
-    if (IsNumberPrime)
+    if (Number > 1)
     {
-        printf("It is a prime number.\n");
+        printf("%lu^%lu", *PrimalitiesArray.Primality, *PrimalitiesArray.PrimalityExponent);
+        for (uint64_t K = 1; K < PrimalitiesArray.PrimalityCount; ++K)
+        {
+            printf(" * %lu^%lu", PrimalitiesArray.Primality[K], PrimalitiesArray.PrimalityExponent[K]);
+        }
     }
     else
     {
-        printf("It is not a prime number.\n");
+        printf("%lu", Number);
     }
 
-    printf("Time-consuming %.3f microseconds.\n", DurationNanoseconds);
+    printf("\n\nTime-consuming %.3f microseconds.\n", DurationNanoseconds);
+
+    printf("\nPress any key to exit...\n");
+    _getch();
 
     return 0;
 }
